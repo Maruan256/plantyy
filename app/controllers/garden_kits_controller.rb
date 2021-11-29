aee9e249f69af1e5cbc2c9a2c64f277c84190fcb
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-11-29 15:33:50 +0100

    search partial

--- app/controllers/garden_kits_controller.rb
+++ app/controllers/garden_kits_controller.rb
@@ -1,6 +1,10 @@
 class GardenKitsController < ApplicationController
   def index
-    @garden_kits = GardenKit.all
+    @garden_kits = GardenKit.order(kit_name: :asc)
+
+    if params[:query].present?
+      @garden_kits = @garden_kits.where('kit_name ILIKE ?', "%#{params[:query]}%")
+    end
   end
 
   def show
---
+++ app/javascript/controllers/search_garden_kits_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ['form', 'input', 'list'];
+
+  connect() {
+    console.log(this.formTarget);
+    console.log(this.inputTarget);
+    console.log(this.listTarget);
+  }
+  update(event) {
+    console.log(event);
+  }
+}
