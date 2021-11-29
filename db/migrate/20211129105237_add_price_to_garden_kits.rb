776d942b64864b574bb01c1cebde88d991a68691
Yi-An Ko <yian80413@gmail.com>
2021-11-29 14:38:25 +0100

    modified migration file

--- db/migrate/20211129105237_add_price_to_garden_kits.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToGardenKits < ActiveRecord::Migration[6.0]
-  def change
-    add_column :garden_kits, :price, :string
-  end
-end
--- db/migrate/20211129115820_delete_price_from_garden_kit.rb
+++
@@ -1,5 +0,0 @@
-class DeletePriceFromGardenKit < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :garden_kits, :price
-  end
-end
