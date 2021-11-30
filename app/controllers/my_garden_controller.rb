95f70b62a900620dfe9187e1148ac09c087f8608
Yi-An Ko <yian80413@gmail.com>
2021-11-30 12:14:00 +0100

    fixed the front end (footer) and the backend of the my_garden show page (now user_garden)

--- app/controllers/my_garden_controller.rb
+++ app/controllers/my_garden_controller.rb
@@ -1,5 +1,4 @@
 class MyGardenController < ApplicationController
-
   def dashboard
     @my_garden = current_user.my_garden
     @my_plants = @my_garden.my_plants
@@ -7,19 +6,16 @@ class MyGardenController < ApplicationController
   end
 
   def create
-    unless @my_garden.present?
-      @my_garden = MyGarden.create(user_id: current_user.id, my_garden_name: current_user.first_name)
-    end
     @garden_kit = GardenKit.find(params[:kit_id])
     @garden_kit.kit_plants.each do |kit_plant|
-      @my_plant = MyPlant.create(plant: kit_plant.plant, my_garden: @my_garden)
+      @my_plant = MyPlant.create(plant: kit_plant.plant, my_garden: current_user.my_garden)
     end
     redirect_to user_garden_path
   end
 
   def update
-    @my_garden = MyGarden.find(params[:id])
-    @garden_kit = GardenKit.find(params[:kit_id])
+    @my_garden = current_user.my_garden
+    @garden_kit = GardenKit.find(params[:id])
     @garden_kit.kit_plants.each do |kit_plant|
       @my_plant = MyPlant.create(plant: kit_plant.plant, my_garden: @my_garden)
     end
