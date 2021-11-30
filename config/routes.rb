00e830cbd039cabe0690085020f5894370c1255f
Yi-An Ko <yian80413@gmail.com>
2021-11-30 10:41:58 +0100

    I fixed the route problem, and Thibault helped with the footer.

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     resources :my_plants, only: [:show, :create, :update]
   end
   get "user_garden", to: "my_garden#dashboard"
+  # user_garden is the name of the route, my_garden is the name of controller, dashboard is the method
+  # this line means you need a "dashboard" method in my_garden method
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
