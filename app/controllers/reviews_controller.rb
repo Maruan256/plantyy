3a20cebe752ab46e0eddac3272533401b2610386
Yi-An Ko <yian80413@gmail.com>
2021-12-01 15:33:20 +0100

    Add review to garden_kit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,12 @@
 class ReviewsController < ApplicationController
+
   def create
-    @garden_kit = GarednKit.find(params[:garden_kit_id])
+    @garden_kit = GardenKit.find(params[:garden_kit_id])
     @review = Review.new(review_params)
     @review.garden_kit = @garden_kit
     # if the garden kit is in the user's garden, the user can leave a review
     # if it isn't in the user's garden,the form simply won't show
+    @review.user = current_user
     if @review.save
       redirect_to garden_kit_path(@garden_kit)
     else
@@ -15,6 +17,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
