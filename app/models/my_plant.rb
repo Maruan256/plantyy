84968cc5878ef6bc1ae524107734faa051289d41
Maruan Paschen <maruanpaschen@gmail.com>
2021-12-01 15:22:03 +0100

    activatian logic

--- app/models/my_plant.rb
+++ app/models/my_plant.rb
@@ -2,7 +2,7 @@ class MyPlant < ApplicationRecord
   belongs_to :plant
   belongs_to :my_garden
 
-
+  before_create :planted
   def planted
     self.water_level = Time.now
     self.fertilizer_level = Time.now
@@ -23,11 +23,11 @@ class MyPlant < ApplicationRecord
 
   def check
     if Time.now - self.water_level >= self.plant.water.to_i
-      "all_good"
+      "all good, your plant is fine."
     elsif Time.now - self.water_level < self.plant.water.to_i && Time.now - self.water_level > 360000
-      "water_soon"
+      "you need to water your plant soon!"
     else
-      "water_now"
+      "Quick! Water! Your plant is dying!"
     end
   end
 end
