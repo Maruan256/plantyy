c32eba88d31e23ec055a3773c19821e1cd4d71f1
Yi-An Ko <yian80413@gmail.com>
2021-12-01 17:46:58 +0100

    finished the review function using AJAX

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,13 +4,15 @@ class ReviewsController < ApplicationController
     @garden_kit = GardenKit.find(params[:garden_kit_id])
     @review = Review.new(review_params)
     @review.garden_kit = @garden_kit
-    # if the garden kit is in the user's garden, the user can leave a review
-    # if it isn't in the user's garden,the form simply won't show
     @review.user = current_user
-    if @review.save
-      redirect_to garden_kit_path(@garden_kit)
-    else
-      render 'garden_kits/show'
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to garden_kit_path(@garden_kit, anchor: "review-#{@review.id}") }
+        format.json
+      else
+        format.html { render 'garden_kits/show' }
+        format.json
+      end
     end
   end
 
